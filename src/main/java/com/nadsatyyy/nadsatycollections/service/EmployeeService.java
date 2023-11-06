c476008df3a20bfcbcee8b8a0d29dc8c2ba7b678
nadsay <nadsatyyy@gmail.com>
2023-11-06 10:43:01 +0300

    Курс 2, Домашка 6

--- src/main/java/com/nadsatyyy/nadsatycollections/service/EmployeeService.java
+++ src/main/java/com/nadsatyyy/nadsatycollections/service/EmployeeService.java
@@ -1,11 +1,15 @@
 package com.nadsatyyy.nadsatycollections.service;
 import com.nadsatyyy.nadsatycollections.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
     Employee add(String firstName, String lastName);
 
     Employee remove(String firstName, String lastName);
 
     Employee find(String firstName, String lastName);
+
+    List<Employee> getAll();
 
 }
